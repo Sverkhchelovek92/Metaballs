efccdfcb76bc4ff8264b7d4e59925170ed0c6cbd
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-01-24 20:13:50 +0300

    test ball

--- script.js
+++ script.js
@@ -9,4 +9,36 @@ function resizeCanvas() {
 resizeCanvas()
 window.addEventListener('resize', resizeCanvas)
 
-console.log(canvas)
+let balls = []
+
+class Ball {
+  constructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.vx = 0
+    this.vy = 0
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+}
+
+balls.push(new Ball(canvas.width / 2, canvas.height / 2, 20, '#ff0000'))
+
+function loop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  balls.forEach((ball) => {
+    ball.draw()
+  })
+
+  requestAnimationFrame(loop)
+}
+
+loop()
