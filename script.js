ba1084fb4533d1abd7035ef271e87bb1ac874582
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-01-28 12:37:47 +0300

    mix colors function

--- script.js
+++ script.js
@@ -100,6 +100,26 @@ class Ball {
   }
 }
 
+function mixColors(col1, col2, w1, w2) {
+  const r1 = parseInt(color1.slice(1, 3), 16)
+  const g1 = parseInt(color1.slice(3, 5), 16)
+  const b1 = parseInt(color1.slice(5, 7), 16)
+
+  const r2 = parseInt(color2.slice(1, 3), 16)
+  const g2 = parseInt(color2.slice(3, 5), 16)
+  const b2 = parseInt(color2.slice(5, 7), 16)
+
+  const total = w1 + w2
+
+  const r = Math.round((r1 * w1 + r2 * w2) / total)
+  const g = Math.round((g1 * w1 + g2 * w2) / total)
+  const b = Math.round((b1 * w1 + b2 * w2) / total)
+
+  return `#${r.toString(16).padStart(2, '0')}${g
+    .toString(16)
+    .padStart(2, '0')}${b.toString(16).padStart(2, '0')}`
+}
+
 balls.push(new Ball(canvas.width / 2, canvas.height / 2, 20, '#ff0000'))
 
 function loop() {
